bda9a26aa356423726a135fb713cf731b24f9948
ArshadSameemdeen <arshadsameemdeen@gmail.com>
2019-04-05 18:27:52 +0530

    asdasd

--- contact.php
+++ contact.php
@@ -18,19 +18,19 @@
             <h3 style="text-align:center;color:#2E4372;"><u>Contact Us</u></h3>
             
             <div class="contact">
-            <h3 style="color:#2E4372;"><u>Kolkata Branch</u></h3>
-            <p><span class="heading">Address - </span>Globsyn Buisness School, IBRAD buisness school, Keshtopur, Kolkata.</p>
-            <p><span class="heading">Tel - </span>033-456892/12</p>
-            <p><span class="heading">Email - </span>kolkatabranch@onlinebank.com</p>
+            <h3 style="color:#2E4372;"><u>Wellawatte Branch</u></h3>
+            <p><span class="heading">Address - </span>Ramakrishna rd, 23/1, Colombo.</p>
+            <p><span class="heading">Tel - </span>011-2341976</p>
+            <p><span class="heading">Email - </span>arshadsameemdeen@gmail.com</p>
             
-             <h3 style="color:#2E4372;"><u>Delhi Branch</u></h3>
-            <p><span class="heading">Address - </span>Globsyn Buisness School, Sector V-A , Malviya Nagar, Delhi.</p>
-            <p><span class="heading">Tel - </span>013-456856/32</p>
-            <p><span class="heading">Email - </span>delhibranch@onlinebank.com</p>
+             <h3 style="color:#2E4372;"><u>Kolpity Branch</u></h3>
+            <p><span class="heading">Address - </span>Kolpity, no.2, Colombo</p>
+            <p><span class="heading">Tel - </span>011-1238753</p>
+            <p><span class="heading">Email - </span>azharanees@gmail.com</p>
             
-             <h3 style="color:#2E4372;"><u>Bangalore Branch</u></h3>
-            <p><span class="heading">Address - </span>Globsyn Buisness School, Near City Center, Kamarthalli, Bangalore.</p>
-            <p><span class="heading">Tel - </span>022-456854/11</p>
-            <p><span class="heading">Email - </span>bangalorebranch@onlinebank.com</p>
+             <h3 style="color:#2E4372;"><u>Pettah Branch</u></h3>
+            <p><span class="heading">Address - </span>Pettah, 1st cross, colombo</p>
+            <p><span class="heading">Tel - </span>011-456854/11</p>
+            <p><span class="heading">Email - </span>akreamakram@onlinebank.com</p>
             </div>
             </div>
